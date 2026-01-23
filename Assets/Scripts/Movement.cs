bee25ef3b1dee5acbfb3633fb448be677a074031
Uni <168829917+UniGC@users.noreply.github.com>
2026-01-23 13:02:36 +0000

    launching arrow grapple thing in mouse direction

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,5 +1,6 @@
 using System.Diagnostics;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -7,7 +8,10 @@ public class Movement : MonoBehaviour
 {
     private Rigidbody2D body;
     private Vector2 power;
-    public Object grapple;
+    public GameObject grapple;
+    private bool grappleLaunched = false;
+    public int grapplePower = 1000;
+    private Vector2 mousePosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,7 +22,18 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (!grappleLaunched) // only rotate grapple when its on the player obv
+        {
+            int grapRotSpeed = 20;
+
+            Vector2 direction = Camera.main.ScreenToWorldPoint(Mouse.current.position.value) - grapple.transform.position;
+
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+            Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+            grapple.transform.rotation = Quaternion.Slerp(grapple.transform.rotation, rotation, grapRotSpeed * Time.deltaTime);
+        } 
     }
 
     private void FixedUpdate()
@@ -26,14 +41,15 @@ public class Movement : MonoBehaviour
         body.AddForce(power); // movement force
     }
 
-    void OnMove(InputValue hey)
+    public void OnMove(InputValue hey)
     {
         power = (10*hey.Get<Vector2>());
     }
 
-    void OnAttack()
+    public void OnAttack()
     {
+        grappleLaunched = true;
         grapple.GetComponent<Rigidbody2D>().simulated = true;
-        grapple.GetComponent<Rigidbody2D>().AddForce(new Vector2 (500,0));
+        grapple.GetComponent<Rigidbody2D>().AddForce(new Vector2 (grapple.transform.right.x, grapple.transform.right.y) * grapplePower);
     }
 }
